c6d2425058294a2b9e7fe249f298999dba890a98
umi <asiessoyeltier1@gmail.com>
2024-06-10 16:28:02 -0500

    [dev] Env setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Flask-API
-Simple API with Flask
+# Why this project
+
+Just to practice Python and maintaining a GitHub repository in order.
+
+## API Functionality
+
+Returns jokes or jokes routines from famous comics
--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,22 @@ import sys
 import logging
 
 from flask import Flask
+from dotenv import  load_dotenv
+
 
 
 def create_app():
 
     app = Flask(__name__)
 
-    #configure env variables
+    # Configure env variables
+    load_dotenv()
+
+    app.config["BASIC_ROUTE"] = os.getenv("BASIC_ROUTE")
 
     # Configure logging
 
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(messages)s", stream=sys.stdout)
-
 
     return app
 
